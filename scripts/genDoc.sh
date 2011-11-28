363ddcc740f7d9ca1f8af3f60d3b0fc59da2d5a1
Bengt Lüers <Bengt.Lueers@gmail.com#>
2011-11-28 18:08:32 +0100

    reworked genDoc.sh

--- scripts/genDoc.sh
+++ scripts/genDoc.sh
@@ -1,11 +1,11 @@
 pwd=`pwd`
 
-if [ ! -d ../VideoNamerDoc ]; then mkdir ../VideoNamerDoc; fi
+if [ ! -d ../../VideoNamerDoc ]; then mkdir ../../VideoNamerDoc; fi
 
 cd ../../VideoNamerDoc
 
 git init
-git remote add origin git@github.com:bigben87/VideoNamer.git &> /dev/null
+git remote add origin https://bigben87@github.com/bigben87/VideoNamer.git &> /dev/null
 
 git fetch origin
 git checkout gh-pages
@@ -13,7 +13,7 @@ git checkout gh-pages
 mv source/index.rst source/index.bck
 rm -rf source/*.rst
 mv source/index.bck source/index.rst
-sphinx-apidoc -o source/ ../VideoNamer
+sphinx-apidoc -o source/ $pwd
 make html
 
 git symbolic-ref HEAD refs/heads/gh-pages
